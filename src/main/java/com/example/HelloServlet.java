e0133b541256129450369b16affd0eb9bd375192
Bhargav P R <bhargavpr99@gmail.com>
2025-09-30 12:04:44 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,6 +1,6 @@
 package com.example;
 
-public class HelloWorld {
+public class HelloServlet {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins and Maven!");
     }
