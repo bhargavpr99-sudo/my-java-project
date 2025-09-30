b640bd118cdf4414ccd926172e033c3f013fc7db
Bhargav P R <bhargavpr99@gmail.com>
2025-09-30 14:36:37 +0530

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -1,7 +1,19 @@
 package com.example;
 
-public class HelloServlet {
-    public static void main(String[] args) {
-        System.out.println("Hello from Jenkins and Maven!");
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Jenkins, Maven, and Tomcat!</h1>");
     }
 }
